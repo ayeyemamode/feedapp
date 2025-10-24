ba731f120ef5a521bbcb43b4a0b9891f579c3d21
aquibajani <aquib.ajani@obsidi.com>
2025-10-24 15:26:55 -0400

    Add UserService

---
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -0,0 +1,28 @@
+package com.bptn.feedapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bptn.feedapp.jdbc.UserBean;
+import com.bptn.feedapp.jdbc.UserDao;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserDao userDao;
+	
+	public List<UserBean> listUsers() {
+		return this.userDao.listUsers();
+	}
+	
+	public UserBean findByUsername(String username) {
+		return this.userDao.findByUsername(username);
+	}
+	
+	public void createUser(UserBean user) {
+		this.userDao.createUser(user);
+	}
+}
