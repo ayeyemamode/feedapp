1a609920b41a3a51855899bc06d522d22e9b1954
Mamode Ayeye <ayeyemamode@gmail.com>
2025-10-28 09:46:00 -0400

    adding Profile JPA Entity

--- src/main/java/com/bptn/feedapp/jpa/User.java
+++ src/main/java/com/bptn/feedapp/jpa/User.java
@@ -1,21 +1,26 @@
 package com.bptn.feedapp.jpa;
-
 import java.io.Serializable;
 import java.sql.Timestamp;
-
+import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
-
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 @Entity
 @Table(name="\"User\"")
-public class User implements Serializable {
+   public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -35,6 +40,7 @@ public class User implements Serializable {
 	@JsonProperty(access = Access.WRITE_ONLY)
 	private String password;
 	
+	
 	private String phone;
 	
 	@Column(name="\"emailId\"", unique=true)
@@ -46,86 +52,95 @@ public class User implements Serializable {
 	@Column(name="\"createdOn\"")
 	private Timestamp createdOn;
 	
-	public User() {
-	}
-
+	@JsonInclude(Include.NON_NULL)
+	@OneToOne(mappedBy="user", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	private Profile profile;
+	@JsonIgnore
+	@OneToMany(mappedBy="user")
+	private List<Feed> feeds;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="user")
+	private List<FeedMetaData> feedMetaData;
+	
 	public Integer getUserId() {
 		return userId;
 	}
-
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 	public String getEmailId() {
 		return emailId;
 	}
-
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
-
 	public Boolean getEmailVerified() {
 		return emailVerified;
 	}
-
 	public void setEmailVerified(Boolean emailVerified) {
 		this.emailVerified = emailVerified;
 	}
-
 	public Timestamp getCreatedOn() {
 		return createdOn;
 	}
-
 	public void setCreatedOn(Timestamp createdOn) {
 		this.createdOn = createdOn;
 	}
-
+	public Profile getProfile() {
+		return profile;
+	}
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	public List<Feed> getFeeds() {
+		return feeds;
+	}
+	public void setFeeds(List<Feed> feeds) {
+		this.feeds = feeds;
+	}
+	public List<FeedMetaData> getFeedMetaData() {
+		return feedMetaData;
+	}
+	public void setFeedMetaData(List<FeedMetaData> feedMetaData) {
+		this.feedMetaData = feedMetaData;
+	}
+	public User() {
+	}
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", username="
 				+ username + ", password=" + password + ", phone=" + phone + ", emailId=" + emailId + ", emailVerified="
 				+ emailVerified + ", createdOn=" + createdOn + "]";
 	}
-	
 }
