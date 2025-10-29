d682297b48bfb11befe73fe43c89f731834d074e
Mamode Ayeye <ayeyemamode@gmail.com>
2025-10-29 14:32:18 -0400

    FeedApp - domain package

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -1,4 +1,5 @@
 package com.bptn.feedapp.service;
+
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.bptn.feedapp.jpa.User;
@@ -7,27 +8,40 @@ import java.util.List;
 import java.util.Optional;
 import java.sql.Timestamp;
 import java.time.Instant;
+
 @Service
 public class UserService {
-	@Autowired
-	UserRepository userRepository;
-	
-	public List<User> listUsers() {
-		return this.userRepository.findAll();
-	}
-	public Optional<User> findByUsername(String username) {
-		return this.userRepository.findByUsername(username);
-	}
-	public void createUser(User user)  {
-		this.userRepository.save(user);
-	}
-	
-	public User signup(User user){
-		user.setUsername(user.getUsername().toLowerCase());
-		user.setEmailId(user.getEmailId().toLowerCase());
-		user.setEmailVerified(false);
-		user.setCreatedOn(Timestamp.from(Instant.now()));
-		this.userRepository.save(user);
-		return user;
-	}
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    public List<User> listUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return this.userRepository.findByUsername(username);
+    }
+
+    public void createUser(User user) {
+        this.userRepository.save(user);
+    }
+
+    public User signup(User user) {
+        user.setUsername(user.getUsername().toLowerCase());
+        user.setEmailId(user.getEmailId().toLowerCase());
+        user.setEmailVerified(false);
+        user.setCreatedOn(Timestamp.from(Instant.now()));
+
+        // Save the user to the database
+        this.userRepository.save(user);
+
+        // Send verification email
+        this.emailService.sendVerificationEmail(user);
+
+        return user;
+    }
 }
