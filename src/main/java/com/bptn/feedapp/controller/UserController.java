d405f93d2c37b811e797c4f4e4d2a7a9c4c5fab3
Mamode Ayeye <ayeyemamode@gmail.com>
2025-10-30 16:06:40 -0400

    adding  FeedApp - Reset Password - UserController

--- src/main/java/com/bptn/feedapp/controller/UserController.java
+++ src/main/java/com/bptn/feedapp/controller/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import static org.springframework.http.HttpStatus.OK;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @CrossOrigin(exposedHeaders = "Authorization")
 @RestController
@@ -98,5 +99,12 @@ public class UserController {
 	        logger.debug("Sending Reset Password Email, emailId: {}", emailId);
 	        
 	        this.userService.sendResetPasswordEmail(emailId);
+	}
+	@PostMapping("/reset")
+	public void passwordReset(@RequestBody JsonNode json) {
+
+	    logger.debug("Resetting Password, password: {}", json.get("password").asText());
+
+	    this.userService.resetPassword(json.get("password").asText());
 	}
 }
