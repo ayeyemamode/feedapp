675e2327fed163cdff4f4ca2e97c665b9283bc16
Mamode Ayeye <ayeyemamode@gmail.com>
2025-10-30 09:29:43 -0400

    adding FeedApp - Sign Up - UserService

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -8,16 +8,23 @@ import java.util.List;
 import java.util.Optional;
 import java.sql.Timestamp;
 import java.time.Instant;
+import com.bptn.feedapp.exception.domain.EmailExistException;
+import com.bptn.feedapp.exception.domain.UsernameExistException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
 
+	
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private EmailService emailService;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    
     public List<User> listUsers() {
         return this.userRepository.findAll();
     }
@@ -31,17 +38,34 @@ public class UserService {
     }
 
     public User signup(User user) {
+
         user.setUsername(user.getUsername().toLowerCase());
         user.setEmailId(user.getEmailId().toLowerCase());
+
+        this.validateUsernameAndEmail(user.getUsername(), user.getEmailId());
+
         user.setEmailVerified(false);
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
         user.setCreatedOn(Timestamp.from(Instant.now()));
 
-        // Save the user to the database
         this.userRepository.save(user);
 
-        // Send verification email
         this.emailService.sendVerificationEmail(user);
 
         return user;
+  }
+
+        private void validateUsernameAndEmail(String username, String emailId) {
+
+            this.userRepository.findByUsername(username).ifPresent(u -> {
+                throw new UsernameExistException(String.format("Username already exists, %s", u.getUsername()));
+            });
+
+            this.userRepository.findByEmailId(emailId).ifPresent(u -> {
+                throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));
+            });
+
     }
+   
 }
+
