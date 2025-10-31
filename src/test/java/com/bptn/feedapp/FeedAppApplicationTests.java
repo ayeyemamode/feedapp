14aa2830efd557c12ec5f43fb7f3f023650747d2
Mamode Ayeye <ayeyemamode@gmail.com>
2025-10-31 14:40:10 -0400

    adding FeedApp - Coding 2.50: FeedApp - Test

--- src/test/java/com/bptn/feedapp/FeedAppApplicationTests.java
+++ src/test/java/com/bptn/feedapp/FeedAppApplicationTests.java
@@ -2,12 +2,19 @@ package com.bptn.feedapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.DisplayName;
+import org.h2.tools.Server;
+import org.springframework.boot.test.mock.mockito.MockBean;
+	@SpringBootTest
+	@ActiveProfiles("test")
+	class FeedAppApplicationTests {
+		@MockBean
+		Server server;
+	   @DisplayName("Demo Test")
+	   @Test
+	   void contextLoads() {
+	   }
 
-@SpringBootTest
-class FeedAppApplicationTests {
-
-	@Test
-	void contextLoads() {
 	}
 
-}
