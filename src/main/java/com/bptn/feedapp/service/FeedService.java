9d849f6549309f063dffdb232292c37d87b2f4d1
Mamode Ayeye <ayeyemamode@gmail.com>
2025-10-31 11:19:51 -0400

    adding FeedApp - FeedApp - Get User Feeds - FeedService

--- src/main/java/com/bptn/feedapp/service/FeedService.java
+++ src/main/java/com/bptn/feedapp/service/FeedService.java
@@ -13,7 +13,10 @@ import com.bptn.feedapp.exception.domain.UserNotFoundException;
 import com.bptn.feedapp.jpa.Feed;
 import com.bptn.feedapp.jpa.User;
 import com.bptn.feedapp.exception.domain.FeedNotFoundException;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import com.bptn.feedapp.domain.PageResponse;
 @Service
 public class FeedService {
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -39,4 +42,15 @@ public class FeedService {
 	    return this.feedRepository.findById(feedId)
 	    .orElseThrow(() -> new FeedNotFoundException(String.format("Feed doesn't exist, %d", feedId)));
 	}
+	public PageResponse<Feed> getUserFeeds(int pageNum, int pageSize) {
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();     
+		    
+		User user = this.userRepository.findByUsername(username)
+		                .orElseThrow(()-> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+		            
+		Page<Feed> paged = this.feedRepository.findByUser(user, PageRequest.of(pageNum, pageSize, Sort.by("feedId").descending()));
+		    
+		return new PageResponse<Feed>(paged);
+		}
 }
